164743d41830c8b1a659349ac7ea1fedf382496d
carltofen1 <patrickpozsgai2@gmail.com>
2025-12-09 11:24:08 -0500

    a

--- src/components/ScrollRestoration.jsx
+++ src/components/ScrollRestoration.jsx
@@ -1,11 +1,20 @@
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function ScrollRestoration() {
     const { pathname } = useLocation();
 
-    useEffect(() => {
+    useLayoutEffect(() => {
+        // Desactivar restauración automática del navegador
+        if ('scrollRestoration' in window.history) {
+            window.history.scrollRestoration = 'manual';
+        }
+
+        // Forzar scroll arriba inmediatamente
         window.scrollTo(0, 0);
+        document.documentElement.scrollTo(0, 0);
+        document.body.scrollTo(0, 0);
+
     }, [pathname]);
 
     return null;
