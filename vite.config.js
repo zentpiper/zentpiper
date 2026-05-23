af1f179a7a0707b51a123bbd40832696fdf8c609
carltofen1 <zentpiper@gmail.com>
2026-05-23 12:14:59 -0500

    l

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Plugin to make CSS non-render-blocking
-function cssAsyncPlugin() {
-  return {
-    name: 'css-async',
-    enforce: 'post',
-    transformIndexHtml(html) {
-      // Convert render-blocking CSS links to async loading
-      // Only in production build
-      return html.replace(
-        /<link rel="stylesheet" crossorigin href="(\/assets\/[^"]+\.css)">/g,
-        `<link rel="stylesheet" href="$1" media="print" onload="this.media='all'" crossorigin>
-    <noscript><link rel="stylesheet" href="$1" crossorigin></noscript>`
-      );
-    },
-  };
-}
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cssAsyncPlugin()],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
